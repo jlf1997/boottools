a2738092259819aba092ff779d5b2d07861ea49e
yangxiang <675866753@qq.com>
2018-06-01 17:27:58 +0800

    v1

--- iot-data-centor/src/main/java/com/cimr/api/history/controller/HistoryRealDataController.java
+++ iot-data-centor/src/main/java/com/cimr/api/history/controller/HistoryRealDataController.java
@@ -3,8 +3,6 @@ package com.cimr.api.history.controller;
 import java.util.List;
 import java.util.Map;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +10,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cimr.api.code.model.Terminal_1_Info;
-import com.cimr.api.history.model.Terminal_1_Info_History;
 import com.cimr.api.history.service.RealDataSignalHistoryService;
 
 import io.swagger.annotations.Api;
@@ -35,7 +31,7 @@ public class HistoryRealDataController {
 	 * @param terid
 	 * @return
 	 */
-	@ApiOperation(value = "根据信号id 查询对应终端的所有历史数据", notes = "terid 传输为all时 表示查询所有终端"			
+	@ApiOperation(value = "根据信号id 查询对应终端的所有历史数据", notes = "查询所有字段"			
 			)	  
 	@ApiImplicitParams({ 
 		@ApiImplicitParam(paramType = "path", dataType = "String", name = "singal", value = "信号", required = true),
@@ -65,7 +61,7 @@ public class HistoryRealDataController {
 	 * @param terid
 	 * @return
 	 */
-	@ApiOperation(value = "根据信号id 查询对应终端的所有历史数据", notes = "terid 传输为all时 表示查询所有终端"			
+	@ApiOperation(value = "根据信号id 查询对应终端的所有历史数据", notes = "可通过fields指定需要的字段，为空时查询所有字段"			
 			)	  
 	@ApiImplicitParams({ 
 		@ApiImplicitParam(paramType = "path", dataType = "String", name = "singal", value = "信号", required = true),
@@ -97,7 +93,7 @@ public class HistoryRealDataController {
 	 * @param terid
 	 * @return
 	 */
-	@ApiOperation(value = "根据信号id 查询对应终端的所有历史数据", notes = "terid 传输为all时 表示查询所有终端"			
+	@ApiOperation(value = "根据信号id 查询对应终端的所有历史数据", notes = "可通过fields指定需要过滤的字段，为空时查询所有字段"			
 			)	  
 	@ApiImplicitParams({ 
 		@ApiImplicitParam(paramType = "path", dataType = "String", name = "singal", value = "信号", required = true),
@@ -124,52 +120,7 @@ public class HistoryRealDataController {
 	
 	
 	
-//	/**
-//	 * 根据时间段查询获取终端的历史数据
-//	 * @param singal
-//	 * @param beg
-//	 * @param end
-//	 * @return
-//	 */
-//	@ApiOperation(value = "根据信号id 时间段 查询所有终端的历史数据", notes = ""			
-//			)
-//	@ApiImplicitParams({ 
-//		@ApiImplicitParam(paramType = "path", dataType = "String", name = "singal", value = "信号", required = true),
-//		@ApiImplicitParam(paramType = "query", dataType = "Long", name = "beg", value = "开始时间", required = true),
-//		@ApiImplicitParam(paramType = "query", dataType = "Long", name = "end", value = "结束时间", required = true)
-//		}) 
-//	@RequestMapping(value="/app/time/{singal}",method=RequestMethod.GET)
-//	public List<Map<String,Object>> findTersRealDataByTime(@PathVariable("singal") String singal,
-//			@RequestParam Long beg,
-//			@RequestParam Long end
-//			) {
-//		List<Map<String,Object>> list = histroyService.findAllByTime(singal, beg, end);
-//		return list;
-//	}
-//	
-//	
-//	/**
-//	 * 根据时间段查询获取终端的历史数据
-//	 * @param singal
-//	 * @param beg
-//	 * @param end
-//	 * @return
-//	 */
-//	@ApiOperation(value = "根据时间段查询获取终端终端位置信息的历史数据", notes = ""			
-//			)
-//	@ApiImplicitParams({ 
-//		@ApiImplicitParam(paramType = "path", dataType = "String", name = "terminalId", value = "终端编号", required = true),
-//		@ApiImplicitParam(paramType = "query", dataType = "Long", name = "beg", value = "开始时间", required = true),
-//		@ApiImplicitParam(paramType = "query", dataType = "Long", name = "end", value = "结束时间", required = true)
-//		}) 
-//	@RequestMapping(value="/location/{terminalId}/time",method=RequestMethod.GET)
-//	public List<Terminal_1_Info_History> findTersRealDataLocationByTime(
-//			@PathVariable("terminalId") String terminalId,
-//			@RequestParam Long beg,
-//			@RequestParam Long end
-//			) {
-//		List<Terminal_1_Info_History> list = histroyService.findAllLocationByTime(terminalId, beg, end);
-//		return list;
-//	}
+	
+
 
 }
